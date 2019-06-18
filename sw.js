e3d31dc368697059517e3d1a8ed1794ef8dca2cb
Sudarshan <snkoirala45@gmail.com>
2019-06-18 15:36:49 +0300

    getting previously cached assets to enable the offline behaviour

--- sw.js
+++ sw.js
@@ -8,7 +8,8 @@ const assets = [
   '/css/styles.css',
   '/css/materialize.min.css',
   '/img/dish.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 //install service worker
 self.addEventListener('install', evt => {
@@ -29,4 +30,9 @@ self.addEventListener('activate', evt => {
 //fetch event
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
