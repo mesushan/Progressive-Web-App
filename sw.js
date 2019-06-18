6c297bbdeb90d4b8dd39e3e71419521af426719b
Sudarshan <snkoirala45@gmail.com>
2019-06-18 17:15:41 +0300

    cache versioning introduced in the app so that the cached assets are kept up to date

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
 const assets = [
   '/',
   '/index.html',
@@ -25,6 +25,16 @@ self.addEventListener('install', evt => {
 //activate event
 self.addEventListener('activate', evt => {
   //console.log('service worker has been activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 //fetch event
