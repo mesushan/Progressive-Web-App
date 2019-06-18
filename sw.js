8db285335cbd28bacf6d3834499a7a3501bc7b2f
Sudarshan <snkoirala45@gmail.com>
2019-06-18 15:22:41 +0300

    pre-caching the assets for offline mode

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
 //install service worker
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed');
+  //console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 //activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated');
+  //console.log('service worker has been activated');
 });
 
 //fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
 });
