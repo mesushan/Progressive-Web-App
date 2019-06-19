03558da68ee0a52e5ee1edcb5ed738cdec5eb205
Sudarshan <snkoirala45@gmail.com>
2019-06-19 14:08:20 +0300

    adding recipes while offline and syncing with firestore database while going online

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v3';
 const assets = [
   '/',
   '/index.html',
@@ -14,7 +14,7 @@ const assets = [
   '/pages/fallback.html'
 ];
 
-//cache size limit funciton
+//cache size limit function
 const limitCacheSize = (name, size) => {
   caches.open(name).then(cache => {
     cache.keys().then(keys => {
@@ -53,26 +53,27 @@ self.addEventListener('activate', evt => {
 
 //fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches
-      .match(evt.request)
-      .then(cacheRes => {
-        return (
-          cacheRes ||
-          fetch(evt.request).then(fetchRes => {
-            return caches.open(dynamicCacheName).then(cache => {
-              cache.put(evt.request.url, fetchRes.clone());
-              limitCacheSize(dynamicCacheName, 15);
-              return fetchRes;
-            });
-          })
-        );
-      })
-      .catch(() => {
-        if (evt.request.url.indexOf('.html') > -1) {
-          return caches.match('/pages/fallback.html');
-        }
-      })
-  );
+  if (evt.request.url.indexOf('firestore.googleapis.com') === -1) {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then(cacheRes => {
+          return (
+            cacheRes ||
+            fetch(evt.request).then(fetchRes => {
+              return caches.open(dynamicCacheName).then(cache => {
+                cache.put(evt.request.url, fetchRes.clone());
+                limitCacheSize(dynamicCacheName, 15);
+                return fetchRes;
+              });
+            })
+          );
+        })
+        .catch(() => {
+          if (evt.request.url.indexOf('.html') > -1) {
+            return caches.match('/pages/fallback.html');
+          }
+        })
+    );
+  }
 });
